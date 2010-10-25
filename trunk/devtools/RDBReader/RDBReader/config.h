421faf9f4b68e94444f241984a8fde06c7b8257a
Tinos1984 <Tinos1984@86551224-105f-1700-0f8a-db5fd8e887f9>
2010-10-25 15:20:18 +0000

    +rdb reader gets now many files from the RDB files (set in config.h wich files you want)

---
+++ trunk/devtools/RDBReader/RDBReader/config.h
@@ -0,0 +1,13 @@
+
+
+bool textscript			= false;
+bool collectionlibrary	= false;
+bool staticapoints2		= false;
+bool rivers				= false;
+bool patrols			= false;
+bool boundedareas		= false;
+bool staticaipoints		= false;
+bool exportPNG			= false;
+bool exportPNG2			= false;
+bool scriptgroup		= false;
+bool animsys			= false;
